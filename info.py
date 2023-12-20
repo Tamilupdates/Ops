569c32be6624c2fe7473e1d189d77b42dec583a5
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2023-12-20 17:02:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'opleech'
-URL = "https://mw-file-bot-ac32422313eb.herokuapp.com//".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://mw-file-bot-ac32422313eb.herokuapp.com//".format(FQDN, PORT)
+URL = "https://mw-file-bot-ac32422313eb.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://mw-file-bot-ac32422313eb.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'opleechbot'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://mw-file-bot-ac32422313eb.herokuapp.com//".format(FQDN)
+    URL = "https://mw-file-bot-ac32422313eb.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://mw-file-bot-ac32422313eb.herokuapp.com//".format(FQDN)
+    URL = "https://mw-file-bot-ac32422313eb.herokuapp.com/".format(FQDN)
 
 
 
