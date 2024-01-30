a02af1545abfa372d5d1766d0c68625495ee9b51
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-01-31 01:32:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,9 @@ from plugins import web_server
 
 import asyncio
 from pyrogram import idle
-from opleechbot import OpleechAngelBot
+from lazybot import OpleechAngelBot
 from util.keepalive import ping_server
-from opleechbot.clients import initialize_clients
+from lazybot.clients import initialize_clients
 
 
 ppath = "plugins/*.py"
